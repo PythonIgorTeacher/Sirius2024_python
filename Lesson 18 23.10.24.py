1e0d8b6cf6a98e0e07664ec11ddf28cd59640fae
iphyperio <iphyperio@gmail.com>
2024-10-23 22:06:33 +0700

    23.10.24

--- Lesson 18 23.10.24.py
+++ Lesson 18 23.10.24.py
@@ -44,36 +44,105 @@
 #         flag = False
 
 
+# import threading
+# import time
+#
+# def target_func(name, stop_thread):
+#     while stop_thread():
+#         print(f'Поток {name} работает')
+#         time.sleep(1)
+#     else:
+#         print('Поток завершил работу')
+#
+# def user_interface(name):
+#     global stop_thread
+#     print(f'```````````````````{name} работает````````````````````',)
+#     print(threading.current_thread().name[:8]) #t2
+#
+#     while t1.is_alive():
+#         x = int(input('Введите значение: '))
+#         if x ==0:
+#             stop_thread = False
+#
+# stop_thread = True
+# t1 = threading.Thread(target = target_func, args=('Первый поток', lambda :stop_thread,))
+# t2 = threading.Thread(target = user_interface, args=('Второй поток',))
+# t1.start()
+# t2.start()
+# t1.join()
+# t2.join()
+#
+# print('Конец работы')
+#
+#
+#
+# #https://www.geeksforgeeks.org/python-different-ways-to-kill-a-thread/
+
+# x = 2
+#
+# import threading
+# lock = threading.Lock()
+# #первый поток
+# lock.acquire()
+# if x !=0:
+#     print(10/x)
+# lock.release()
+# #второй поток
+# ....
+# x-=2
+# ...
+
+
+# import threading
+# counter = 0
+# lock = threading.Lock()
+#
+# def increment():
+#     global counter
+#     with lock: # lock.acquire()
+#         for _ in range(10_000):
+#             counter+=1
+#     #lock.release()
+#
+# threads = [threading.Thread(target = increment) for _ in range(10)]
+# [t.start() for t in threads]
+# [t.join() for t in threads]
+# print(counter)
+
+
+
 import threading
-import time
 
-def target_func(name, stop_thread):
-    while stop_thread():
-        print(f'Поток {name} работает')
-        time.sleep(1)
-    else:
-        print('Поток завершил работу')
-
-def user_interface(name):
-    global stop_thread
-    print(f'```````````````````{name} работает````````````````````',)
-    print(threading.current_thread().name) #t2
-
-    while t1.is_alive():
-        x = int(input('Введите значение: '))
-        if x ==0:
-            stop_thread = False
-
-stop_thread = True
-t1 = threading.Thread(target = target_func, args=('Первый поток', lambda :stop_thread,))
-t2 = threading.Thread(target = user_interface, args=('Второй поток',))
-t1.start()
-t2.start()
-t1.join()
-t2.join()
-
-print('Конец работы')
+lock = threading.Lock()
+f = open('threading.txt','w') #чтобы обнлуить файлик
+f.close()
 
 
+class Writer:
+    def __init__(self):
+        self.lock = threading.Lock()
+    def write(self, filename = 'threading.txt',value='' ):
+        with open(filename,'a') as f:
+            f.write(value+'\n')
 
-#https://www.geeksforgeeks.org/python-different-ways-to-kill-a-thread/
+def thread_func(w:Writer):
+    thread_name = threading.current_thread().name
+    w.lock.acquire()
+    for i in range(10):
+        w.write(value=f'{thread_name}')
+    w.lock.release()
+
+def thread_func(w: Writer):
+    thread_name = threading.current_thread().name
+    w.lock.acquire()
+    for i in range(10):
+        w.write(value=f'{thread_name}')
+    w.lock.release()
+
+w = Writer()
+threads = [threading.Thread(target = thread_func,args=(w, )) for _ in range(10)]
+[t.start() for t in threads]
+[t.join() for t in threads]
+
+
+
